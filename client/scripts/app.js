ac68866bcd044c9c68dbaca76fc068d1400425fd
Renata Maraj <renatavmaraj@gmail.com>
2017-01-26 11:46:17 -0800

    implement render room

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,76 +1,109 @@
-// YOUR CODE HERE:
+  // YOUR CODE HERE:
 var app = {
 
-    server: 'https://api.parse.com/1/classes/messages',
-    username: 'anonymous',
-    roomname: 'lobby',
-    messages: [],
-    lastMessageId: 0,
+   server: 'https://api.parse.com/1/classes/messages',
+   username: 'anonymous',
+   roomname: 'lobby',
+   messages: [],
+   lastMessageId: 0,
 
-    init: function() {
-        // determine the username
-        app.username = window.location.search.substr(10);
+   init: function() {
+       // determine the username
+       app.username = window.location.search.substr(10);
 
-        //Cache jquery selections
-        app.$message = $('#message');
-        app.$message = $('#message');
-        app.$roomSelect = $('#roomSelect');
-        app.$send = $('#send');
-    },
+       //Cache jquery selections
+       app.$message = $('#message');
+       app.$chats = $('#chats');
+       app.$roomSelect = $('#roomSelect');
+       app.$send = $('#send');
 
-    send: function(message) {
-        $.ajax({
-            url: app.server,
-            method: 'POST',
-            data: JSON.stringify(message),
-            success: function() {
-                console.log("this");
-            },
-            error: function() {
-                console.log("Error posting message");
-            }
-        })
-    },
+       //Add listeners
+       app.$send.on('submit', app.handleSubmit)
 
-    fetch: function() {
-        $.ajax({
-            url: app.server,
-            method: 'GET',
-            success: function(data) {
-                console.log("Data has been gotten", data);
-                //Don't do anything if we have nothing to work with
-                if (!data.results || !data.results.length) {
-                    return;
-                }
-                //Store messages for caching later
-                app.messages = data.results;
-                //Only bother updating the DOM if we have a new message
-                var mostRecentMessage = app.message[app.message.length - 1];
+       //Fetch all messages when app starts up
+       app.fetch();
 
-                if (mostRecentMessage.objectId !== app.lastMessageId) {
-                    //clear dom
-                    app.$chats.html('');
+       //Pull new messages
+       // setInterval(function(){
+       //  app.fetch()
+       // }, 3000)
+   },
 
-                    for (var i = 0; i < app.messages.length; i++) {
-                        //Render each individual message
-                        //Create a div to hold the message
-                        var $chat = $('<div class="chat"/>');
-                        //Add in the message data
-                        var $username = $('<span class="username">' + app.messages[i].text + '</span>');
-                        $username.appendTo($chat);
+   send: function(message) {
+       $.ajax({
+           url: app.server,
+           type: 'POST',
+           data: JSON.stringify(message),
+           success: function() {
+               console.log("this");
+           },
+           error: function() {
+               console.log("Error posting message");
+           }
+       });
+   },
 
-                        var $message = $('<br><span>' + app.messages[i].text + '</span>');
-                        $message.appendTo($chat);
-                        //Add the message to the UI
-                        app.$chats.append($chat);
+   fetch: function() {
+       $.ajax({
+           url: app.server,
+           type: 'GET',
+           success: function(data) {
+               console.log("Data has been gotten", data);
+               //Don't do anything if we have nothing to work with
+               if (!data.results || !data.results.length) {
+                   return;
+               }
+               //Store messages for caching later
+               app.messages = data.results;
+               //Only bother updating the DOM if we have a new message
+               var mostRecentMessage = app.messages[app.messages.length - 1];
 
-                    }
-                }
-            },
-            error: function(error) {
-                console.log("Error fetching data")
-            }
-        });
-    }
+               if (mostRecentMessage.objectId !== app.lastMessageId) {
+                   app.renderMessages(app.messages)
+               }
+           },
+           error: function(error) {
+               console.log("Error fetching data")
+           }
+       });
+   },
 
-};
+   renderMessages: function(arrayOfMessages) {
+       //Clear old messages
+       app.clearMessages()
+
+       // for (var i = 0; i < app.arrayOfMessages.length; i++) {
+       //     //Render each individual message
+       //     app.renderMessage(messages[i]);
+       //Refactor to use forEach
+       arrayOfMessages.forEach(app.renderMessage);
+   },
+
+   clearMessages: function() {
+       app.$chats.html('');
+   },
+
+   renderMessage: function(message) {
+    //Create a div to hold the message
+     var $chat = $('<div class="chat"/>');
+     //Add in the message data
+     var $username = $('<span class="username">' + message.username + '</span>');
+     $username.appendTo($chat);
+
+     var $message = $('<br><span>' + message.text + '</span>');
+     $message.appendTo($chat);
+     //Add the message to the UI
+     app.$chats.append($chat);
+   },
+
+   handleSubmit: function(){
+    console.log("inside handle submit")
+    event.preventDefault();
+   },
+
+   renderRoom: function(){
+
+   }
+
+  };
+
